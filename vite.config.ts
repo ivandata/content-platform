fa2f8a1750bc39cc97c67dcf7acf7c596a691a5b
Ivandata <ivandata@users.noreply.github.com>
2025-02-12 13:14:17 +0100

    feat: Add icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import react from '@vitejs/plugin-react'
 import path from 'path';
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
-    })
+    }),
+    svgr()
   ],
   resolve: {
     alias: {
